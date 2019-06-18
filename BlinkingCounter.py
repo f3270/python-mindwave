06dcb89ad036e18c52712ac03d06ca2e30fec9db
faturita <rramele@gmail.com>
2019-06-18 17:38:16 -0300

    Update the file

--- BlinkingCounter.py
+++ BlinkingCounter.py
@@ -3,6 +3,23 @@
 import csv
 import numpy as np
 
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+
+import peakutils
+
+def baseline_als(y, lam, p, niter=10):
+  L = len(y)
+  D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+  w = np.ones(L)
+  for i in range(niter):
+    W = sparse.spdiags(w, 0, L, L)
+    Z = W + lam * D.dot(D.transpose())
+    z = spsolve(Z, w*y)
+    w = p * (y > z) + (1-p) * (y < z)
+  return z
+
+
 results = []
 
 print 'Este programa tiene que ejecutarse con python 2.7!'
@@ -12,7 +29,7 @@ print 'Este programa tiene que ejecutarse con python 2.7!'
 with open('blinking.dat') as inputfile:
     for row in csv.reader(inputfile):
         rows = row[0].split(' ')
-        results.append(rows[0:])
+        results.append(rows[1:])
 
 print 'Longitud del archivo:'+str(len(results))
 
@@ -27,9 +44,24 @@ results = results.astype(int)
 # en relación a las muestras (1:100 serian las muestras) representante
 # del tiempo.
 # La segunda columna, corresponde a: eeg, attention y meditation.
-eeg = results[1:100,0]
+eeg = results[1:,0]
 
 print eeg
+
+eeg = np.ones((64))
+
+eeg = np.arange(64)
+
+print eeg.shape
+
+eeg[32] = 120
+
+eeg[43] = -130
+
+eeg = eeg - baseline_als(eeg,10000,0.5)
+
+#eeg = peakutils.baseline(eeg,1)
+
 
 import matplotlib.pyplot as plt
 fig = plt.figure()
@@ -40,9 +72,22 @@ plt.legend(loc='upper left');
 plt.show()
 
 
+
+
 # El threshold corresponde al limite en amplitud a considerar para discriminar
 # que es un pestañeo de qué no lo es.
-signalthreshold = 60
+signalthreshold = 140
+
+def crest_factor(x):
+    return np.max(np.abs(x))/np.sqrt(np.mean(np.square(x)))
+
+ptp = abs(np.max(eeg)) + abs(np.min(eeg))
+rms = np.sqrt(np.mean(eeg**2))
+cf = crest_factor(eeg)
+
+print ptp
+print rms
+print cf
 
 boolpeaks = np.where( eeg < -signalthreshold  )
 print boolpeaks
